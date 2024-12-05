6b8c9f2ccd4010a2dce8e5860de68fe327e394ab
VandevilleF <flo.vandeville@gmail.com>
2024-12-05 16:50:32 +0100

    Task 1, local test ok

--- markdown2html.py
+++ markdown2html.py
@@ -3,6 +3,20 @@
 import sys
 import os
 
+
+def convert_headings(md_text):
+    """Convert Mardown title to HTML Tag"""
+    html_line = []
+
+    for line in md_text.splitlines():
+        if line.startswith('#'):
+            header_lvl = len(line.split()[0])
+            if 1 <= header_lvl <= 6:
+                content = line.strip("#").strip()
+                html_line.append(f"<h{header_lvl}>{content}</h{header_lvl}>\n")
+    return "".join(html_line)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
@@ -12,13 +26,15 @@ if __name__ == "__main__":
     html_file = sys.argv[2]
 
     if not os.path.exists(md_file):
-        sys.stderr.write("Missing {md_file}\n")
+        sys.stderr.write(f"Missing {md_file}\n")
         exit(1)
 
     else:
         try:
-            with open(html_file, "w") as f:
-                f.write("")
+            with open(md_file, "r") as mardown_f:
+                md_content = mardown_f.read()
+            with open(html_file, "w") as html_f:
+                html_f.write(convert_headings(md_content))
                 exit(0)
         except Exception as e:
             exit(1)
